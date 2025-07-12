38dd19ead302d3da0f2d0aae162162d9dbd79dba
YASENcode0 <easeneaseneasen123@gmail.com>
2025-07-12 17:50:07 +0300

    ui 1

--- client/src/components/SideMnu/SideMnu.jsx
+++ client/src/components/SideMnu/SideMnu.jsx
@@ -1,6 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SideMnu.css";
+import logo from "../../assets/logos/logo192.png";
+
+import { IoIosHome } from "react-icons/io";
 
 export default function SideMnu() {
-   return <div className="side-mnu">SideMnu</div>;
+  const [open, setOpen] = useState(false);
+
+  function ofOn() {
+    setOpen(!open);
+  }
+
+  return (
+    <div className={`side-mnu ${open && "side-mnu-close"}`}>
+      <div className="logo-div">
+        <img src={logo} alt="logo" />
+        <button onClick={ofOn}>c</button>
+      </div>
+      <ul>
+        <li>
+          <span>
+            <IoIosHome />
+          </span>
+          <h3>home</h3>
+        </li>
+        <li>
+          <span>i</span>
+          <h3>home</h3>
+        </li>
+        <li>
+          <span>i</span>
+          <h3>home</h3>
+        </li>
+        <li>
+          <span>i</span>
+          <h3>home</h3>
+        </li>
+      </ul>
+
+      <div className="sign-button">
+        <button>login</button>
+      </div>
+    </div>
+  );
 }
