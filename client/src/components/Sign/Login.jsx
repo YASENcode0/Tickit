19a326f6049908138fd2ecd4a50fc0c72fff8c25
YASENcode0 <easeneaseneasen123@gmail.com>
2025-07-22 21:06:07 +0300

    login form validate

--- client/src/components/Sign/Login.jsx
+++ client/src/components/Sign/Login.jsx
@@ -1,21 +1,85 @@
-import React from "react";
-import "./Sign.css"
+import React, { useState } from "react";
+import "./Sign.css";
 
 export default function Login() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [inputErrs, setInputErrs] = useState({});
+
+  function inputsLengthValidate(field, length) {
+    console.log(field);
+
+    // if (field.target.value < 0) {
+    //   setInputErrs({
+    //     ...inputErrs,
+    //     [field.target.name]: `${length} is required`,
+    //   });
+    //   return
+    // }
+
+    if (field.target.value.length < length) {
+      setInputErrs({
+        ...inputErrs,
+        [field.target.name]: `min length ${length} deg`,
+      });
+    } else {
+      setInputErrs({ ...inputErrs, [field.target.name]: "" });
+    }
+  }
+
+  function inputsRequiredValidate(input, name) {
+    console.log(input);
+    if (!input) {
+      setInputErrs({ ...inputErrs, [name]: `${name} is required` });
+    }
+  }
+
+  function login(e) {
+    e.preventDefault();
+    inputsRequiredValidate(email, "email");
+    inputsRequiredValidate(password, "password");
+  }
+
   return (
     <div className="login">
       <h1>log in</h1>
       <form>
         <div className="form-unit">
           <label>email</label>
-          <input type="email" placeholder="email" required min="6"/>
+          <input
+            type="email"
+            placeholder="email"
+            name="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+              inputsLengthValidate(e, 5);
+            }}
+            value={email}
+          />
+          <span>{inputErrs?.email}</span>
         </div>
         <div className="form-unit">
           <label>password</label>
-          <input type="password" placeholder="password" required min="6"/>
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+              inputsLengthValidate(e, 10);
+            }}
+            value={password}
+          />
+          <span>{inputErrs?.password}</span>
         </div>
         <hr />
-        <button>login</button>
+        <button
+          onClick={(e) => {
+            login(e);
+          }}
+        >
+          login
+        </button>
       </form>
     </div>
   );
